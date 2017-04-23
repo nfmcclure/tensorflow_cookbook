241c001c1b9a7b6cbaedb482dd39651511d81314
Jim Thompson <jimthompson5802@aol.com>
2017-04-23 08:13:26 -0400

    restructured python module to match notebook

--- 03_Linear_Regression/07_Implementing_Elasticnet_Regression/07_elasticnet_regression.py
+++ 03_Linear_Regression/07_Implementing_Elasticnet_Regression/07_elasticnet_regression.py
@@ -14,16 +14,34 @@ import numpy as np
 import tensorflow as tf
 from sklearn import datasets
 from tensorflow.python.framework import ops
+
+###
+# Set up for TensorFlow
+###
+
 ops.reset_default_graph()
 
 # Create graph
 sess = tf.Session()
+
+###
+# Obtain data
+###
 
 # Load the data
 # iris.data = [(Sepal Length, Sepal Width, Petal Length, Petal Width)]
 iris = datasets.load_iris()
 x_vals = np.array([[x[1], x[2], x[3]] for x in iris.data])
 y_vals = np.array([y[0] for y in iris.data])
+
+###
+# Setup model
+###
+
+# make results reproducible
+seed = 13
+np.random.seed(seed)
+tf.set_random_seed(seed)
 
 # Declare batch size
 batch_size = 50
@@ -52,6 +70,10 @@ loss = tf.expand_dims(tf.add(tf.add(tf.reduce_mean(tf.square(y_target - model_ou
 my_opt = tf.train.GradientDescentOptimizer(0.001)
 train_step = my_opt.minimize(loss)
 
+###
+# Train model
+###
+
 # Initialize variables
 init = tf.global_variables_initializer()
 sess.run(init)
@@ -69,9 +91,17 @@ for i in range(1000):
         print('Step #' + str(i+1) + ' A = ' + str(sess.run(A)) + ' b = ' + str(sess.run(b)))
         print('Loss = ' + str(temp_loss))
 
+###
+# Extract model results
+###
+
 # Get the optimal coefficients
 [[sw_coef], [pl_coef], [pw_ceof]] = sess.run(A)
 [y_intercept] = sess.run(b)
+
+###
+# Plot results
+###
 
 # Plot loss over time
 plt.plot(loss_vec, 'k-')
@@ -79,3 +109,4 @@ plt.title('Loss per Generation')
 plt.xlabel('Generation')
 plt.ylabel('Loss')
 plt.show()
+
